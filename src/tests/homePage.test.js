37891698cefcafc6ec1393847752e1c49a16355d
Rafael Assad <rafaassadeng@gmail.com>
2021-02-20 14:11:28 -0300

    testes

--- src/tests/homePage.test.js
+++ src/tests/homePage.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import HomePage from '../pages/homePage';
+ // import { render } from '@testing-library/react';
+import NewDeck from '../pages/newDeck.js';
+import renderWithRouter from '../services/renderWithRouter.js';
 
 describe('homepage', () => {
   test('should render with default props', () => {
-    const { container } = render(<HomePage />);
+    const { container } = renderWithRouter(<NewDeck />);
 
     expect(container).toBeInTheDocument();
   });
